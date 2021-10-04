d5952a3438b83f23cce2c2ae4da46206503dd3ad
jwlacy17 <89400812+jwlacy17@users.noreply.github.com>
2021-10-04 09:29:19 -0400

    Add files via upload

---
+++ dataPart/ref (4).sql
@@ -0,0 +1,105 @@
+
+--
+-- Database: `ref`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assignment`
+--
+
+DROP TABLE IF EXISTS `assignment`;
+CREATE TABLE IF NOT EXISTS `assignment` (
+  `assignmentID` int(11) NOT NULL AUTO_INCREMENT,
+  `position` varchar(256) DEFAULT NULL,
+  `status` varchar(256) DEFAULT NULL,
+  `refID` int(11) DEFAULT NULL,
+  `gameID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`assignmentID`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `assignment`
+--
+
+INSERT INTO `assignment` (`assignmentID`, `position`, `status`, `refID`, `gameID`) VALUES
+(1, 'X', 'assigned ', 1, 1),
+(2, 'Y', 'Unassigned', 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `game`
+--
+
+DROP TABLE IF EXISTS `game`;
+CREATE TABLE IF NOT EXISTS `game` (
+  `gameID` int(11) NOT NULL AUTO_INCREMENT,
+  `field` varchar(256) DEFAULT NULL,
+  `time` time DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `assignmentID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`gameID`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `game`
+--
+
+INSERT INTO `game` (`gameID`, `field`, `time`, `date`, `assignmentID`) VALUES
+(1, 'fieldName1', '05:00:00', '2021-10-04', 1),
+(2, 'fieldName2', '06:00:00', '2021-10-04', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `referee`
+--
+
+DROP TABLE IF EXISTS `referee`;
+CREATE TABLE IF NOT EXISTS `referee` (
+  `refID` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(256) DEFAULT NULL,
+  `lastName` varchar(256) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `refGrade` int(11) DEFAULT NULL,
+  `refRating` int(11) DEFAULT NULL,
+  PRIMARY KEY (`refID`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `referee`
+--
+
+INSERT INTO `referee` (`refID`, `firstName`, `lastName`, `age`, `refGrade`, `refRating`) VALUES
+(1, 'Jake', 'Doe', 25, 87, 95),
+(2, 'Jane ', 'Doe', 34, 98, 95);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(256) DEFAULT NULL,
+  `lastName` varchar(256) DEFAULT NULL,
+  `role` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`userID`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userID`, `firstName`, `lastName`, `role`) VALUES
+(1, 'John', 'Doe ', 'Assignor '),
+(2, 'John ', 'Smith', 'user');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
